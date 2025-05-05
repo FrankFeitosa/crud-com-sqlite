5daf4bb2bc7a14616dfb4bca83d1e052c82fd336
FrankFeitosa <feitosfrank22@gmail.com>
2025-05-05 14:27:53 -0300

    feat: add route by user ID

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createAllUsers, deleteUsers, getAllUsers, updateAllUsers } from '../controllers/userController.js';
+import { createAllUsers, deleteUsers, getAllUsers, getIdUsers, updateAllUsers } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
+router.get("/:id", getIdUsers);
 router.post("/post", createAllUsers);
 router.put("/u/:id", updateAllUsers)
 router.delete("/r/:id", deleteUsers);
